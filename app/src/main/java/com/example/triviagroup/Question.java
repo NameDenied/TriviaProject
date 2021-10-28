52fae693792cebca80932a79953529ecb9300be1
samueloh1145 <johansionoh@gmail.com>
2021-10-28 08:16:29 -0500

    Q DONE L:DDDD

--- app/src/main/java/com/example/triviagroup/Question.java
+++ app/src/main/java/com/example/triviagroup/Question.java
@@ -11,33 +11,39 @@ public class Question {
         answer = new String[questionA.length-1];
         correct = questionA[1];
         String[] temp = new String[questionA.length-1];
+
+        for(int i = 0; i < answer.length; i++ ){
+            temp[i] = questionA[i+1];
+        }
         int[] used = new int[questionA.length-1];
         for(int i = 0; i < answer.length; i++){
-            temp[i] = questionA[i+1];
+           // temp[i] = questionA[i+1];
             boolean found = true;
             while(found){
-                int rand = (int)(Math.random()*questionA.length);
+                int rand = (int)(Math.random()* answer.length);
                 found = false;
                 for(int v = 0; v < i; v++){
                     if(used[v]==rand){
                         found = true;
                     }
+
                 }
                 if(!found){
                     used[i] = rand;
-                    answer[0] = temp[rand];
+                    answer[i] = temp[rand];
                 }
             }
         }
     }
+    public String getCorrectAnswer(){return correct;}
+
     public String getQuestion(){
         return question;
     }
     public String[] getAnswer(){
         return answer;
     }
-    public String getCorrectAnswer(){
-        return correct;
+
     }
 
-}
+
--- app/src/main/java/com/example/triviagroup/questionScreen.java
+++ app/src/main/java/com/example/triviagroup/questionScreen.java
@@ -13,6 +13,7 @@ public class questionScreen extends AppCompatActivity {
     int pointsEarned = 0;
     String[] tst = {"What is life", "george", "god", "death"};
     Question[] askQ = {new Question(tst),new Question(tst),new Question(tst),new Question(tst),new Question(tst)};
+    int ind = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +33,10 @@ public class questionScreen extends AppCompatActivity {
             index = 0;
         }
 //        String[][] correctA = {{"Tony Stark", "6","The ten rings","25","Wakanda", "Loki","Clint Barton","God of Mischief"}};
-        String[][] marvel = {{"What is Iron Man’s real name?", "Tony Stark", "Peter Parker", "Steve Rogers"}, {"How many infinity stones does Thanos need?", "3", "4", "6"}, {"What is the main weapon used by Shang Chi and his father?", "The ten rings", "Infinity Gauntlet", "Mjollnir"}, {"How many Marvel movies have been made?", "23", "18", "25"},{"What is the setting of Black Panther?", "Australia", "Wakanda", "China", "Asgard"}, {"What is the name of Thor’s brother?", "Deadpool", "Loki", "Odin"}, {"What is Hawkeye's real name?", "Bart CLinton", "Clint Barton", "Cole Philson"}, {"What is Loki's title?", "God of Mischief", "God of Evil", "God of Tricks"}};
+        String[][] marvel = {{"What is Iron Man’s real name?", "Tony Stark", "Peter Parker", "Steve Rogers"}, {"How many infinity stones does Thanos need?", "6", "4", "3"}, {"What is the main weapon used by Shang Chi and his father?", "The ten rings", "Infinity Gauntlet", "Mjollnir"}, {"How many Marvel movies have been made?", "25", "18", "23"},{"What is the setting of Black Panther?", "Wakanda", "Australia", "China", "Asgard"}, {"What is the name of Thor’s brother?", "Loki", "Deadpool", "Odin"}, {"What is Hawkeye's real name?", "Bart CLinton", "Clint Barton", "Cole Philson"}, {"What is Loki's title?", "God of Mischief", "God of Evil", "God of Tricks"}};
         //String[][][] Questions = {{{"What is Iron Man’s real name?", "Peter Parker", "Tony Stark", "Steve Rogers"}, {"How many infinity stones does Thanos need?", "3", "4", "6"}, {"What is the main weapon used by Shang Chi and his father?", "The ten rings", "Infinity Gauntlet", "Mjollnir"}, {"How many Marvel movies have been made?", "23", "18", "25"},{"What is the setting of Black Panther?", "Australia", "Wakanda", "China", "Asgard"}, {"What is the name of Thor’s brother?", "Deadpool", "Loki", "Odin"}, {"What is Hawkeye's real name?", "Bart CLinton", "Clint Barton", "Cole Philson"}, {"What is Loki's title?", "God of Mischief", "God of Evil", "God of Tricks"}}};
         Question[][] Questions = {{new Question(marvel[0]), new Question(marvel[1]), new Question(marvel[2]), new Question(marvel[3]),new Question(marvel[4]),new Question(marvel[5]),new Question(marvel[6]), new Question(marvel[7])}};
+        Question[] questionOrder = new Question[marvel.length];
         int len = Questions[index].length;
         System.out.println(len);
 //        System.out.println(index);
@@ -66,13 +68,32 @@ public class questionScreen extends AppCompatActivity {
 //            }
 //            count--;
 //        }
+        while(count > 0){
+            boolean found = true;
+            int chosen = (int)(Math.random()*len);
+            while(found){
+                found = false;
+                for(int i = 0; i < useQ.length; i++){
+                    if(useQ[i] == chosen){
+                        found = true;
+                    }
+                }
+                if(!found){
+                    askQ[5-count] = Questions[index][chosen];
+                    useQ[5-count] = chosen;
+                }else{
+                    chosen = (int)(Math.random()*len);
+                }
+            }
+            count--;
+        }
 //        for(int i = 0; i < askQ.length; i++){
 //            System.out.print(askQ[i] + " : " + useQ[i]);
 //        }
 //        System.out.println();
         //get the spinner from the xml.
 //        String[] test = {"What is Iron Man’s real name?", "Peter Parker", "Tony Stark", "Steve Rogers"};
-        //Question lol = new Question(test);
+//        Question lol = new Question(test);
 
         Spinner dropdown = findViewById(R.id.qspinner);
         //create a list of items for the spinner.
@@ -93,8 +114,20 @@ public class questionScreen extends AppCompatActivity {
 //            }
 //        }
         ThemeText.setText(askQ[0].getQuestion());
-        String[] items = new String[]{askQ[0].getAnswer()[0], askQ[0].getAnswer()[1], askQ[0].getAnswer()[2]};
-        System.out.println(askQ.getCorrectAnswer());
+        String[] items = new String[askQ[0].getAnswer().length];
+        //String[] items = new String[]{askQ[0].getAnswer()[0], askQ[0].getAnswer()[1], askQ[0].getAnswer()[2], askQ[0].getAnswer()[3]};
+        if(items.length == 3){
+            items[0] = askQ[0].getAnswer()[0];
+            items[1] = askQ[0].getAnswer()[1];
+            items[2] = askQ[0].getAnswer()[2];
+        }else if(items.length == 4){
+            items[0] = askQ[0].getAnswer()[0];
+            items[1] = askQ[0].getAnswer()[1];
+            items[2] = askQ[0].getAnswer()[2];
+            items[3] = askQ[0].getAnswer()[3];
+        }
+
+        System.out.println(askQ[0].getCorrectAnswer());
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
 //        ThemeText.setText(askQ[0].getQuestion());
@@ -108,6 +141,34 @@ public class questionScreen extends AppCompatActivity {
 
 
     public void submitQuestion(View v){
+        Spinner dropdown = findViewById(R.id.qspinner);
+        String SelectedAnswer = String.valueOf(dropdown.getSelectedItem());
+        System.out.println(SelectedAnswer);
+        System.out.println(askQ[ind].getCorrectAnswer());
+        if(askQ[ind].getCorrectAnswer().equals(SelectedAnswer)){
+            System.out.println("CORRECTOMUNDO");
+            pointsEarned = pointsEarned+1;
+        }else{
+            System.out.println("OOF YOU SUCK");
+        }
+        ind = ind + 1;
+        TextView ThemeText = findViewById(R.id.themeText);
+        ThemeText.setText(askQ[ind].getQuestion());
+        String[] items = new String[askQ[ind].getAnswer().length];
+        //String[] items = new String[]{askQ[0].getAnswer()[0], askQ[0].getAnswer()[1], askQ[0].getAnswer()[2], askQ[0].getAnswer()[3]};
+        if(items.length == 3){
+            items[0] = askQ[ind].getAnswer()[0];
+            items[1] = askQ[ind].getAnswer()[1];
+            items[2] = askQ[ind].getAnswer()[2];
+        }else if(items.length == 4){
+            items[0] = askQ[ind].getAnswer()[0];
+            items[1] = askQ[ind].getAnswer()[1];
+            items[2] = askQ[ind].getAnswer()[2];
+            items[3] = askQ[ind].getAnswer()[3];
+        }
 
+        System.out.println(askQ[ind].getCorrectAnswer());
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 }
